35c11a82a45a1cb419379285436e35d8fe83f364
Superfelicia <89514597+Superfelicia@users.noreply.github.com>
2021-11-30 16:21:42 +0100

    added index.js

---
+++ index.js
@@ -0,0 +1,51 @@
+// FIRST ASSIGNMENT
+
+// Calculate the area of two triangles
+// Compare the values of the result to see which one has the biggest area
+// Code one example for each test data that you are provided
+// One case (code example) for the first test data, one for the second and so on.
+// Use template literals to output the result to the console
+
+
+// Formula for calculating the area of a triangle:
+// area = (base * height) /2
+
+// OBS! In this case you actually have to use the parentheses
+// when doing the area calculation.
+// Answer the question why JavaScript needs the parentheses in this
+// case? You can put your answer as plain text when you hand in the
+// assignment.
+
+
+// TEST DATA
+// Case 1
+// triangleOne: base = 10cm, height = 13cm
+// triangleTwo: base = 16,5cm, height = 20,3cm
+
+// Case 2
+// triangleTwo: base = 16,5cm, height = 20,3cm
+// triangleTwo: base = 20,3cm, height = 16,5cm
+
+// Case 3
+// triangleTwo: base = 7,8cm, height = 5,6cm
+// triangleTwo: base = 9,3cm, height = 12,4cm
+
+// Print the total area for each triangle together with the result of which
+// one that has the biggest are. 
+// Example: `Triangle One has an bigger area of xx cm2 and Triangle Two has
+// an area of xx cm2. The triangle with the biggest area is Triangle xx.`
+
+// Code an example for each case of test data.
+// Hand in a link to your GitHub repo for your assignment and don't
+// forget to answer the question above.
+
+
+
+
+// 1. Make sure I know how to calculate the area of a triangle
+// 2. Calculate one triangles are and make sure the formula works and the
+// correct result shows in the console.
+// 3. Compare both results and output it to the console. Make sure to output
+// which one that has the biggest area. 
+// 4. 
+
